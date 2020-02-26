801d01229cf4c43c46579ec3e8b184a0d6414394
Carolina Vega <carolina.vega@sunwebgroup.com>
2020-02-26 11:01:31 +0100

    Initial Commit. New .net core 3.1 web app

---
+++ olottech-ci-cd.web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace olottech_ci_cd.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
